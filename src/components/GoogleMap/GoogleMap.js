f364b32849d7dc09526fa0375f9c50bbeb8a0e5f
Vlad Konovalchuk <berlitio600@gmail.com>
2019-01-22 22:42:53 +0200

    Add proto of Infowindow

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,17 +1,43 @@
 import React, {PureComponent} from 'react';
 import styles from "./GoogleMap.module.scss";
-import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
 import PropTypes from "prop-types";
 
 
 export class MapContainer extends PureComponent {
-
     state = {
         center: {
             lat: 40.7831,
             lng: -73.9712
         },
-        zoom: 14
+        zoom: 18,
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+    };
+    onMarkerClick = (props, marker, e) => {
+        console.log('clicked marker');
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true,
+
+        })
+    };
+
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    };
+    onInfoWindowClose = () => {
+        this.setState({
+            showingInfoWindow: false,
+            activeMarker: null
+        })
     };
 
     render() {
@@ -30,26 +56,48 @@ export class MapContainer extends PureComponent {
                     m={18}
                     style={style}
                     google={this.props.google}
-                    zoom={14}
+                    zoom={15}
                     initialCenter={{
                         lat: activeProperty.latitude,
                         lng: activeProperty.longitude
                     }}
+                    onClick={this.onMapClick}
+
                 >
-                    {properties.map(item => (<Marker key={item._id}
-                                                     position={{lat: item.latitude, lng: item.longitude}}/>))}
+                    {properties.map(item => (
+                        <Marker key={item._id}
+                                position={{lat: item.latitude, lng: item.longitude}}
+                                onClick={this.onMarkerClick}
+                                name={'Current location'}
+                        >
+                            <InfoWindow
+                                marker={this.state.activeMarker}
+                                visible={this.state.showingInfoWindow}
+                                onClose={this.onInfoWindowClose}
+                            >
+                                <div style={style}>
+                                    <h1>{item.address}</h1>
+                                </div>
+                            </InfoWindow>
+                        </Marker>))}
                 </Map>
             </div>
         );
     }
 }
 
-MapContainer.propTypes = {
+MapContainer
+    .propTypes = {
     properties: PropTypes.array.isRequired,
     activeProperty: PropTypes.object.isRequired,
-    filteredProperties: PropTypes.func.isRequired,
+    filteredProperties: PropTypes.array.isRequired,
     isFiltering: PropTypes.bool.isRequired
 };
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyA9_abOu_dTqGFHeMwAHA1Hf5VheHbpaQ4'
-})(MapContainer);
+})
+
+(
+    MapContainer
+)
+;
